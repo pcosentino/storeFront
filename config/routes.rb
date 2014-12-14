5c4a2b660a91ae1be37c9e7de1833950f7e5b323
Paolo Cosentino <cosepaol88@gmail.com>
2014-12-14 15:02:19 -0600

    add catalog, items and items-details

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   mount Shoppe::Engine => "/shoppe"
+
+  get 'product/:permalink' => 'products#show', :as => 'product'
+  root :to => 'products#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
