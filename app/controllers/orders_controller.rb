79c6319d55d795aff92163f3831da0c021cbff14
Paolo Cosentino <cosepaol88@gmail.com>
2014-12-15 18:20:47 -0600

    added shopping cart to application

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,8 @@
+class OrdersController < ApplicationController
+	def destroy
+		current_order.destroy
+		session[:order_id] = nil
+		redirect_to root_path, :notice => "Basket emptied successfully!"		
+	end
+
+end
