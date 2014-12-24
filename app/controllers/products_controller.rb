dbff900fe267642be4781190094b42404c5bcb97
Paolo Cosentino <cosepaol88@gmail.com>
2014-12-24 15:54:29 -0600

    Shopping cart header partial added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,6 +12,6 @@ class ProductsController < ApplicationController
   def buy
     @product = Shoppe::Product.find_by_permalink!(params[:permalink])
     current_order.order_items.add_item(@product, 1)
-    redirect_to product_path(@product.permalink), :notice => "Product has been added successfuly!"
+    redirect_to product_path(@product.permalink), :notice => "Product has been added successfully!"
   end
 end
